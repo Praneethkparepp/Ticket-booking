2c11fa8f0b04a86ea61970c0df9fd832de334aa9
amdocsprasoon <prasoonv@amdocs.com>
2025-04-22 23:38:08 +0530

    Added a line , Learning the working of git revert

--- app/src/main/java/ticket/booking/App.java
+++ app/src/main/java/ticket/booking/App.java
@@ -18,6 +18,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Running Train Booking System");
         System.out.println("Just a testing line, ignore this");
+        System.out.println("Learning the working of git revert");
         Scanner scanner = new Scanner(System.in);
         int option = 0;
         UserBookingService userBookingService;
